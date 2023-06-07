42bbc88a57b705ff999ca4477dbaa972db4ab548
shawn <1443584075@qq.com>
2023-06-07 17:00:09 +0800

    inites

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import categoriesJson from "../../categories.json";
+import CategoryItem from "../category-item/category-item.component";
+import "./directory.styles.scss";
+
+const Directory = () => {
+  return (
+    <div className="categories-container">
+      {categoriesJson.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+};
+
+export default Directory;
